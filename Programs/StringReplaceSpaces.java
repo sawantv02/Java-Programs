90d5eb629c1014aa2dae4027e102247eb0721ac0
vishakha <sawant.vi@husky.neu.edu>
2017-05-10 10:37:03 -0400

    replace space with char

---
+++ Programs/StringReplaceSpaces.java
@@ -0,0 +1,26 @@
+package Programs;
+
+public class StringReplaceSpaces {
+
+	public static void main(String args[]){
+		String s="My First Page";
+		System.out.println("Replace space with '%20'-----------------");
+		System.out.println(replaceSpace(s));
+		
+	}
+	
+	public static String replaceSpace(String s){
+		String replace="%20";
+		char ch[]=s.toCharArray();
+		StringBuffer bf=new StringBuffer();
+		for(char c:ch){
+			if(c==' ')
+				bf.append(replace);
+			else
+				bf.append(c);
+			
+		}
+		
+		return bf.toString();
+	}
+}
